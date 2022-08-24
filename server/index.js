1e049aef1d88f50c256e4507a77e5241c5a75415
ramdas.shinde@cloudstrats.com <ramdas.shinde@cloudstrats.com>
2022-08-24 11:56:58 +0530

    firstcommit

---
+++ server/index.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const cors = require('cors');
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
+const app = express();
+const http = require("http");
+const socket = require("socket.io");
+const koa = require("koa");
+
+const allowedOrigins = ['http://192.168.1.13:3000', 'http://192.168.1.13:5000'];
+
+var whitelist = ['http://192.168.1.13:3000','http://192.168.1.13:5000']
+
+// PORT configuration
+const PORT = process.env.PORT || 5000;
+
+// IP configuration
+const IP = process.env.IP || 5000;
+
+
+
+require("dotenv").config();
+
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+app.use(express.json());
+
+
+
+mongoose
+  .connect(process.env.ATLAS_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+
+
+// const server = app.listen(process.env.PORT,   () =>
+//   console.log(`Server started on ${process.env.PORT}`)
+// );
+
+const server = app.listen(PORT, IP, ()  => {
+  console.log(`The Server is running at: http://192.168.1.13:${PORT}`);
+});
+
+
+
+
+
+
+
+
+const io = socket(server, {
+  cors: {
+    origin: ['http://192.168.1.13:3000'],
+    credentials: true,
+    optionsSuccessStatus: 200
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
+});
